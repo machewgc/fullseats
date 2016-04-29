4dadaa3ca0a6e1406dfd8b987080e0a3c5b4bc06
Matthew Lee <matt+matt@>
2016-04-28 22:22:41 -0500

    adjusted faq

--- js/custom.js
+++ js/custom.js
@@ -53,6 +53,7 @@ $(window).resize(function () {
 
 });
 
+
 $(window).on("orientationchange",function(){
   var currentLocation = window.location;
   if (currentLocation.toString().indexOf("#") !== -1) {
@@ -65,4 +66,51 @@ $(window).on("orientation",function(){
   if (currentLocation.toString().indexOf("#") !== -1) {
     document.getElementById("search").click();
   }
+});
+
+
+$(window).on("orientationchange",function(){
+  // Select and loop the container element of the elements you want to equalise
+  $('.enforce-jumbotron-height').each(function(){
+
+    // Cache the highest
+    var highestBox = 0;
+
+    // Select and loop the elements you want to equalise
+    $('.jumbotron', this).each(function(){
+
+      // If this box is higher than the cached highest then store it
+      if($(this).height() > highestBox) {
+        highestBox = $(this).height();
+      }
+
+    });
+
+    // Set the height of all those children to whichever was highest
+    $('.jumbotron',this).height(highestBox);
+
+  });
+});
+
+$(window).on("orientation",function(){
+  // Select and loop the container element of the elements you want to equalise
+  $('.enforce-jumbotron-height').each(function(){
+
+    // Cache the highest
+    var highestBox = 0;
+
+    // Select and loop the elements you want to equalise
+    $('.jumbotron', this).each(function(){
+
+      // If this box is higher than the cached highest then store it
+      if($(this).height() > highestBox) {
+        highestBox = $(this).height();
+      }
+
+    });
+
+    // Set the height of all those children to whichever was highest
+    $('.jumbotron',this).height(highestBox);
+
+  });
 });
